3d7a924a06e028ce2d81bbf658027f89a791b105
mickeyKaiz <ravindukd@gmail.com>
2020-01-19 06:53:42 +0530

    ff

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+// Import and configure the Firebase SDK
+// These scripts are made available when the app is served or deployed on Firebase Hosting
+// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
+importScripts('/__/firebase/7.6.2/firebase-app.js');
+importScripts('/__/firebase/7.6.2/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
+
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
